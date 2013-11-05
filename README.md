aa6181239116135dc1448ff8aba9a64d57b5b249
Justin Reidy <jmreidy@rzrsharp.net>
2013-11-04 18:10:24 -0800

    Remove tunnel tracking logic
    The Sauce Tunnel JAR has logic to close existing tunnels that match an
    identifier. This wrapper now just creates a new tunnel with a default
    identifier, and doesn't deal with trying to kill any open tunnels with
    the same identifier (the JAR can handle that).
    
    Likewise, calling `stop` will now just kill the single tunnel.
    
    Fixes #3.

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 sauce-tunnel
 ============
 
-A Node.js wrapper around the Saucelabs tunnel jar. 
+A Node.js wrapper around the Saucelabs tunnel jar.
 
-This code is extracted from [grunt-saucelabs](https://github.com/axemclion/grunt-saucelabs) by [axemclion](https://github.com/axemclion), with the grunt-specific parts removed. 
+This code is extracted from
+[grunt-saucelabs](https://github.com/axemclion/grunt-saucelabs) by
+[axemclion](https://github.com/axemclion), with the grunt-specific parts
+removed.
 
-It was extracted into its own module to avoid duplication between grunt-saucelabs, [grunt-mocha-webdriver](https://github.com/grunt-mocha-webdriver), and any future Node module that may need it.
+It was extracted into its own module to avoid duplication between
+grunt-saucelabs,
+[grunt-mocha-webdriver](https://github.com/grunt-mocha-webdriver), and any
+future Node module that may need it.
+
+## CHANGELOG
+
+### v1.1
+- Remove all the logic surrounding tracking open tunnels, killing existing
+tunnels, and tunnel timeouts. (#3)
+
+
--- index.js
+++ index.js
@@ -9,15 +9,14 @@ var EventEmitter = require('events').EventEmitter;
 
 module.exports = SauceTunnel;
 
-function SauceTunnel(user, key, identifier, tunneled, tunnelTimeout) {
-  EventEmitter.call(this)
+function SauceTunnel(user, key, identifier, tunneled) {
+  EventEmitter.call(this);
   this.user = user;
   this.key = key;
-  this.identifier = identifier;
+  this.identifier = identifier || 'Tunnel'+new Date().getTime();
   this.tunneled = tunneled;
-  this.tunnelTimeout = tunnelTimeout;
   this.baseUrl = ["https://", this.user, ':', this.key, '@saucelabs.com', '/rest/v1/', this.user].join("");
-};
+}
 
 util.inherits(SauceTunnel, EventEmitter);
 
@@ -66,29 +65,25 @@ SauceTunnel.prototype.getTunnels = function(callback) {
   });
 };
 
-SauceTunnel.prototype.killAllTunnels = function(callback) {
+SauceTunnel.prototype.killTunnel = function(callback) {
   if (!this.tunneled) {
     return callback();
   }
-  var me = this;
 
-  me.emit('verbose:debug', 'Trying to kill all tunnels');
-  this.getTunnels(function(tunnels) {
-    (function killTunnel(i) {
-      if (i >= tunnels.length) {
-        setTimeout(callback, 1000 * 5);
-        return;
-      }
-      me.emit('log:writeln', util.format("=> Killing tunnel %s", tunnels[i]));
-      request({
-        method: "DELETE",
-        url: me.baseUrl + "/tunnels/" + tunnels[i],
-        json: true
-      }, function() {
-        killTunnel(i + 1);
-      });
-    }(0));
-  });
+  this.emit('verbose:debug', 'Trying to kill tunnel');
+  request({
+    method: "DELETE",
+    url: this.baseUrl + "/tunnels/" + this.identifier,
+    json: true
+  }, function (err, resp, body) {
+    if (!err) {
+      this.emit('verbose:debug', 'Tunnel Closed');
+    }
+    else {
+      this.emit('log:error', 'Error closing tunnel');
+    }
+    callback(err);
+  }.bind(this));
 };
 
 SauceTunnel.prototype.start = function(callback) {
@@ -96,31 +91,9 @@ SauceTunnel.prototype.start = function(callback) {
   if (!this.tunneled) {
     return callback(true);
   }
-  this.getTunnels(function(tunnels) {
-    if (!tunnels) {
-      me.emit('verbose:error', "=> Could not get tunnels for Sauce Labs. Still continuing to try connecting to Sauce Labs".inverse);
-    }
-    if (tunnels && tunnels.length > 0) {
-      me.emit('log:writeln', util.format("=> Looks like there are existing tunnels to Sauce Labs, need to kill them. TunnelID:%s", tunnels));
-      (function waitForTunnelsToDie(retryCount) {
-        if (retryCount > 5) {
-          me.emit('verbose:writeln', util.format("=> Waited for %s retries, now trying to shut down all tunnels and try again", retryCount));
-          me.killAllTunnels(function() {
-            me.start(callback);
-          });
-        } else {
-          me.emit('verbose:debug', util.format("=> %s. Sauce Labs tunnels already exist, will try to connect again %s milliseconds.", retryCount, me.tunnelTimeout / 5));
-          setTimeout(function() {
-            waitForTunnelsToDie(retryCount + 1);
-          }, me.tunnelTimeout / 5);
-        }
-      }(0));
-    } else {
-      me.emit('verbose:writeln', "=> Sauce Labs trying to open tunnel".inverse);
-      me.openTunnel(function(status) {
-        callback(status);
-      });
-    }
+  this.emit('verbose:writeln', "=> Sauce Labs trying to open tunnel".inverse);
+  this.openTunnel(function(status) {
+    callback(status);
   });
 };
 
@@ -128,7 +101,7 @@ SauceTunnel.prototype.stop = function(callback) {
   if (this.proc) {
     this.proc.kill();
   }
-  this.killAllTunnels(function() {
-    callback();
+  this.killTunnel(function(err) {
+    callback(err);
   });
 };
